707443d4b3e721bceb3e7a3897fa96fa8f01809c
fredrikssongustav <gqstav@gmail.com>
2020-03-08 20:17:32 +0100

    feat: make update function support text area events

--- src/FindAndReplace/FindAndReplace.tsx
+++ src/FindAndReplace/FindAndReplace.tsx
@@ -16,10 +16,10 @@ export const FindAndReplace: React.FC = () => {
     setForThis(findAndReplaceMethod(replaceThis, withThis, forThis));
   };
 
-  const updateField = <T extends string> (
-    e: React.ChangeEvent<HTMLInputElement>,
-    setter: (value: T) => void): void => {
-    setter(`${e.target.value}` as T);
+  const updateField = <T1 extends string, T2 extends HTMLInputElement | HTMLTextAreaElement> (
+    e: React.ChangeEvent<T2>,
+    setter: (value: T1) => void): void => {
+    setter(`${e.target.value}` as T1);
   };
 
   useEffect(() => {
@@ -31,24 +31,24 @@ export const FindAndReplace: React.FC = () => {
 
 
   return (
-    <div>
+    <div className="flex-container">
       <input
         data-testid="replace-this-input"
         placeholder="I want to replace this"
         value={replaceThis}
-        onChange={(e): void => updateField<string>(e, setReplaceThis)}
+        onChange={(e): void => updateField<string, HTMLInputElement>(e, setReplaceThis)}
       />
       <input
         data-testid="with-this-input"
         placeholder="With this"
         value={withThis}
-        onChange={(e): void => updateField<string>(e, setWithThis)}
+        onChange={(e): void => updateField<string, HTMLInputElement>(e, setWithThis)}
       />
-      <input
+      <textarea
         data-testid="for-this-input"
         placeholder="In this text field"
         value={forThis}
-        onChange={(e): void => updateField<string>(e, setForThis)}
+        onChange={(e): void => updateField<string, HTMLTextAreaElement>(e, setForThis)}
       />
       <button
         data-testid="make-replace-button"
